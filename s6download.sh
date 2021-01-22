ade845847f5c0e5c6dfe1857483ec8b29f27e194
Froschie <github@froschie.de>
2021-01-22 16:32:19 +0100

    test

--- s6download.sh
+++ s6download.sh
@@ -1,14 +1,27 @@
-arch=$(uname -m)
-echo $arch
-
-if [ "$arch" == 'x86_64' ];
+if [ -z ${TARGETPLATFORM} ]
 then
-echo "X64 Architecture"
-curl -o /tmp/s6overlay.tar.gz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-amd64.tar.gz
-fi
-
-if [ "$arch" == 'armv7l' ];
-then
-echo "Arm architecture"
-curl -o /tmp/s6overlay.tar.gz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-armhf.tar.gz
+    arch=$(uname -m)
+    echo "Arch: "${arch}
+    if [ "${arch}" == 'x86_64' ];
+    then
+    echo "X64 Architecture"
+    curl -o /tmp/s6overlay.tar.gz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-amd64.tar.gz
+    fi
+    if [ "${arch}" == 'armv7l' ];
+    then
+    echo "Arm architecture"
+    curl -o /tmp/s6overlay.tar.gz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-armhf.tar.gz
+    fi
+else
+    echo "Platform: "${TARGETPLATFORM}
+    if [ "${TARGETPLATFORM}" == 'linux/amd64' ];
+    then
+    echo "X64 Architecture"
+    curl -o /tmp/s6overlay.tar.gz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-amd64.tar.gz
+    fi
+    if [ "${TARGETPLATFORM}" == 'linux/arm/v7' ];
+    then
+    echo "Arm architecture"
+    curl -o /tmp/s6overlay.tar.gz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-armhf.tar.gz
+    fi
 fi
