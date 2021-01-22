559654dfd520f3991339ddb1c89628d2c40005c5
Froschie <github@froschie.de>
2021-01-22 17:49:22 +0100

    change back to arm v7

--- s6download.sh
+++ s6download.sh
@@ -19,7 +19,7 @@ else
     echo "X64 Architecture"
     curl -o /tmp/s6overlay.tar.gz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-amd64.tar.gz
     fi
-    if [ "${TARGETPLATFORM}" == 'linux/arm/v6' ];
+    if [ "${TARGETPLATFORM}" == 'linux/arm/v7' ];
     then
     echo "Arm architecture"
     curl -o /tmp/s6overlay.tar.gz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-armhf.tar.gz
